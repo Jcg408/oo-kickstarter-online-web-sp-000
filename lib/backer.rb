dec54332fa041d285873ad8e9315a023eacccfb8
Jessica Gladstone <jcg321@comcast.net>
2018-11-27 16:35:38 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,5 +3,11 @@ class Backer
   
   def initialize (name)
     @name = name
-    @backed_projects = []
+    @backed_projects = [] 
+  end
+  
+  def back_project (project) 
+    @backed_projects << project
+    project.backers << self
+  end
 end
